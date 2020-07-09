41590194dce2ce2713da370204e139e2c0b59a22
Nautilusshell <39021950+nautilusshell@users.noreply.github.com>
2020-07-09 15:44:25 +0800

    update

--- services/commons.go
+++ services/commons.go
@@ -12,5 +12,21 @@ var logger = loggers.Log
 
 func GetResource() ([]models.AIFrameworkItem, []models.DeviceItem, error) {
 
-	return nil, nil, nil
+	fw := make([]models.AIFrameworkItem, 0)
+	devices := make([]models.DeviceItem, 0)
+
+	engine := models.AIFrameworkItem{
+		Name:   "tensorflow",
+		Engine: "tf_withtools:1.15",
+	}
+
+	device := models.DeviceItem{
+		DeviceType: "npu",
+		Avail:      1,
+	}
+
+	fw = append(fw, engine)
+	devices = append(devices, device)
+
+	return fw, devices, nil
 }
