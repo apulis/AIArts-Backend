463a2c97cc0bfb4752acdd60260db7b18917fe8a
dragon0486 <869071540@qq.com>
2020-07-10 17:32:24 +0800

    update config

--- services/annotations.go
+++ services/annotations.go
@@ -13,6 +13,7 @@ var BackendUrl string
 func GetProjects() ([]models.Project, error) {
 	BackendUrl = configs.Config.Anno.BackendUrl
 	resp, err := grequests.Get(BackendUrl+"/api/projects", nil)
+	logger.Info(resp)
 	var projects []models.Project
 	json.Unmarshal(resp.Bytes(),&projects)
 	return projects,err
