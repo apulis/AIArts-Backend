65b0b1619f71f3196d7eb4edaf8d55ee6089be3c
dragon0486 <869071540@qq.com>
2020-07-13 17:48:27 +0800

    update logic

--- models/annotations.go
+++ models/annotations.go
@@ -1,62 +1,62 @@
 package models
 
 type Project struct {
-	ProjectId string	`json: projectId`
-	Name string	`json: name`
-	Info string	`json: info`
-	Role string	`json: role`
+	ProjectId string	`json:"projectId"`
+	Name string	`json:"name"`
+	Info string	`json:"info"`
+	Role string	`json:"role"`
 }
 
 type DataSet struct {
-	DataSetId string	`json: dataSetId`
-	Name string	`json: name`
-	Info string	`json: info`
-	Type string	`json: type`
-	Labels []Label `json: labels`
-	DataSetBindId string `json: dataSetBindId`
-	DataSetPath string `json: dataSetPath`
+	DataSetId string	`json:"dataSetId"`
+	Name string	`json:"name"`
+	Info string	`json:"info"`
+	Type string	`json:"type"`
+	Labels []Label `json:"labels"`
+	DataSetBindId string `json:"dataSetBindId"`
+	DataSetPath string `json:"dataSetPath"`
 }
 
 type ProjectsReq struct {
 	Successful string
 	Msg string
-	Projects []Project `json: projects`
+	Projects []Project `json:"projects"`
 	TotalCount int `json:"totalCount"`
 }
 
 type DatasetsReq struct {
 	Successful string
 	Msg string
-	Datasets []DataSet `json: datasets`
+	Datasets []DataSet `json:"datasets"`
 	TotalCount int `json:"totalCount"`
 }
 
 type DatasetReq struct {
 	Successful string
 	Msg string
-	Info DataSet `json: info`
+	Info DataSet `json:"info"`
 }
 
 type UpdateDataSet struct {
-	Name string	`json: name`
-	Info string `json: info`
-	Type string	`json: type`
-	DataSetBindId string `json: dataSetBindId`
-	DataSetPath string `json: dataSetPath`
-	Labels []Label `json: labels`
+	Name string	`json:"name"`
+	Info string `json:"info"`
+	Type string	`json:"type"`
+	DataSetBindId string `json:"dataSetBindId"`
+	DataSetPath string `json:"dataSetPath"`
+	Labels []Label `json:"labels"`
 }
 
 type Label struct {
-	Id int				`json: id`
-	Name string			`json: name`
-	Type string 		`json: type`
-	Supercategory string	`json: supercategory`
+	Id int				`json:"id"`
+	Name string			`json:"name"`
+	Type string 		`json:"type"`
+	Supercategory string	`json:"supercategory"`
 }
 
 type LabelReq struct {
 	Successful string
 	Msg string
-	Annotations interface{}	`json: annotations`
+	Annotations interface{}	`json:"annotations"`
 }
 
 type TasksList struct {
@@ -69,13 +69,13 @@ type TasksList struct {
 type NextTask struct {
 	Successful string
 	Msg string
-	Next interface{} `json:next`
+	Next interface{} `json:"next"`
 }
 
 type OneTask struct {
 	Successful string
 	Msg string
-	Annotations interface{} `json:annotations`
+	Annotations interface{} `json:"annotations"`
 }
 
 type QueryStringParameters struct {
