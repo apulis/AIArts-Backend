4c45c51c740e9c0c9eaeaedb4eca9b4373789fda
dragon0486 <869071540@qq.com>
2020-07-28 19:21:20 +0800

    update

--- models/annotations.go
+++ models/annotations.go
@@ -133,8 +133,7 @@ func (queryStringParameters QueryStringParameters) GetPageSize() int {
 }
 
 func (queryStringParameters QueryStringParameters) GetName() string {
-	logger.Info(strings.Trim(strings.Trim(strings.Trim(queryStringParameters.Name, "\n"), " "), "\n"))
-	return queryStringParameters.Name
+	return strings.TrimSpace(queryStringParameters.Name)
 }
 
 type QueryStringParamInterface interface {
