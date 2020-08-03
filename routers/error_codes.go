8bd171e55e19d77cd7ada3caa0cf44d9e18ffaa2
wscjxky <619677632@qq.com.com>
2020-08-03 16:26:28 +0800

    新增数据集isTranslated 获取用户所有可用数据集

--- routers/error_codes.go
+++ routers/error_codes.go
@@ -28,7 +28,9 @@ const (
 	UPLOAD_TEMPDIR_FULL_CODE = 30009
 	//无法删除正在使用的数据集
 	DATASET_IS_STILL_USE_CODE = 30010
-
+	// dataset
+	//访问标注平台获取数据集出错
+	FAILED_FETCH_ANNOTATION_CODE = 30011
 	// modelset
 	//创建评估训练任务出错
 	CREATE_EVALUATION_FAILED_CODE = 30701
