6328f782c16ff15d1a46bdee045bcf4873506dd1
wscjxky <619677632@qq.com>
2020-09-14 13:10:05 +0800

    取消启动文件限制

--- models/training_model.go
+++ models/training_model.go
@@ -36,9 +36,9 @@ func ValidHomePath(userName, path string) bool {
 
 func (t *Training) ValidatePathByUser(userName string) (bool, string) {
 
-	if !strings.HasSuffix(t.StartupFile, ".py") {
-		return false, "启动文件非python"
-	}
+	//if !strings.HasSuffix(t.StartupFile, ".py") {
+	//	return false, "启动文件非python"
+	//}
 
 	//if !ValidHomePath(userName, t.StartupFile) {
 	//	return false, "启动文件路径错误"
