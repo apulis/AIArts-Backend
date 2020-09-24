8d2563f2fae47ef7a7924f08cb8686d13c2b390b
250lth <gang250lth@gmail.com>
2020-09-24 11:03:56 +0800

    fix docker save sudo

--- services/saved_img.go
+++ services/saved_img.go
@@ -32,7 +32,7 @@ func CreateSavedImage(name, version, description, jobId, username string, isPriv
 	version = trimImageName(version)
 	imageName := trimImageName(username) + "_" + name + ":" + version
 	fullName := configs.Config.PrivateRegistry + imageName
-	cmd := "sudo docker commit " + containerId + " " + fullName
+	cmd := "docker commit " + containerId + " " + fullName
 	logger.Info("Running docker-commit command: ", cmd)
 	res, err := runSShCmd(hostIp, cmd)
 	logger.Info("Run ssh command docker-commit result: ", res)
@@ -41,7 +41,7 @@ func CreateSavedImage(name, version, description, jobId, username string, isPriv
 	}
 
 	// 执行docker push
-	cmd = "sudo docker push " + fullName
+	cmd = "docker push " + fullName
 	logger.Info("Running docker-push command: ", cmd)
 	res, err = runSShCmd(hostIp, cmd)
 	logger.Info("Run ssh command docker-push result: ", res)
