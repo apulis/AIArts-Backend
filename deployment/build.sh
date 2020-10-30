45cf3fe90693d15e573156bae5c7ea28ad2a741e
root <root@DESKTOP-ONU8CEI.localdomain>
2020-10-30 14:58:24 +0800

    perf(build):won't tag image if build fail

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd ..
-docker build . -t aiarts:1.0 -f deployment/Dockerfile
-docker tag aiarts:1.0 apulistech/aiarts:1.0
+docker build . -t aiarts:1.0 -f deployment/Dockerfile && docker tag aiarts:1.0 apulistech/aiarts:1.0
 #docker push apulistech/aiarts:1.0
