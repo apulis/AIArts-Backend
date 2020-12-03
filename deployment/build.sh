a025fab5cf695c19cb142c51ef9a7be681eedb3b
nautilusshell <bifeng.peng@apulis.com>
2020-12-03 10:31:44 +0800

    修改编译文件

--- deployment/build.sh
+++ deployment/build.sh
@@ -4,7 +4,7 @@ set -x
 
 # 镜像名和代码分支配置
 image_name=aiarts-backend
-tag_name=rc3
+tag_name=rc5
 branch_name=v1.2.0
 image_fullname=${image_name}:${branch_name}-${tag_name}
 
@@ -14,7 +14,8 @@ harbor_fullname=${harbor_path}${image_fullname}
 local_harbor_fullname=harbor.sigsus.cn:8443/sz_gongdianju/apulistech/dlworkspace_aiarts-backend:1.0
 
 # 拷贝到其它机器
-remote_ip=219.133.167.42
+#remote_machine=(219.133.167.42 52080)
+remote_machine=(192.168.1.176 22)
 tar_name=`sed 's|:|_|g' <<< ${image_fullname}`.tar
 
 
@@ -38,7 +39,7 @@ function postbuild() {
 
 
     ## 开发环境
-    scp -P 52080 ${tar_name} root@${remote_ip}:/tmp
+    scp -P ${remote_machine[1]} ${tar_name} root@${remote_machine[0]}:/tmp
 
     ## 导入镜像包并重新打印标签、push 本地harbor等等
     cmd="cd /tmp; docker load < ${tar_name};
@@ -49,12 +50,7 @@ function postbuild() {
              ./deploy.py kubernetes start ${image_name};
     "
     echo $cmd
-    ssh -p 52080 root@${remote_ip} $cmd
-
-
-
-    ## 测试环境
-    #scp -P 5122 ${new_image}.tar root@10.31.3.106:/tmp
+    ssh -p ${remote_machine[1]} root@${remote_machine[0]} $cmd
 }
 
 
