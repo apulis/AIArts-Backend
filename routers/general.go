72de7e8cde48969fe833dcb9e9cf56fe74dc3ae8
xiaoxiao.liu <xiaoxiao.liu@apulis.com>
2020-12-03 18:28:19 +0800

    模型训练：优化模型训练vcName查询

--- routers/general.go
+++ routers/general.go
@@ -197,9 +197,6 @@ func getJobSummary(c *gin.Context) error {
 	var err error
 	var req GetJobSummaryReq
 
-	if len(req.VCName) == 0 {
-		req.VCName = models.DefaultVcName
-	}
 
 	if err = c.Bind(&req); err != nil {
 		return ParameterError(err.Error())
